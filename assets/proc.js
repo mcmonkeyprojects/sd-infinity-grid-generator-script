3d5fa16e9c13538e15b2344c369511e4bbe4d453
skfoo <44075232+skfoo@users.noreply.github.com>
2023-10-14 13:28:02 -0700

    Add input for making GIF frames in reverse order

--- assets/proc.js
+++ assets/proc.js
@@ -871,6 +871,12 @@ function makeGif() {
         let actualUrl = imgPath.join('/') + '.' + rawData.ext;
         images.push(actualUrl);
     }
+
+    if (document.getElementById('makegif_direction').value == "backwards") {
+        images.reverse();
+        imgPath.reverse();
+    }
+
     let encoder = new GIFEncoder();
     encoder.setRepeat(0);
     encoder.setDelay(1000 / speed);

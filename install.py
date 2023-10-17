fd3acb3e8b09e61cb2a3139714481e6cce893c6d
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-10-17 13:26:19 -0700

    fix redundant installer call of pyyaml-include

--- install.py
+++ install.py
@@ -2,5 +2,5 @@ import launch
 
 if not launch.is_installed("yaml"):
     launch.run_pip("install pyyaml", "pyyaml for Infinity Grid Script")
-if not launch.is_installed("pyyaml-include"):
+if not launch.is_installed("yamlinclude"):
     launch.run_pip("install pyyaml-include", "pyyaml-include for Infinity Grid Script")

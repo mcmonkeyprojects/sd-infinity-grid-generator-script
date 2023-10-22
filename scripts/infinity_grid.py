3f3abfeff92d1d0a6a92f304e8ba907bf5d16ec2
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-10-22 03:58:20 -0700

    fix URL for absolute paths and add a notice

--- scripts/infinity_grid.py
+++ scripts/infinity_grid.py
@@ -364,15 +364,18 @@ class Script(scripts.Script):
         def get_page_url_text(file):
             if file is None:
                 return "(...)"
+            notice = ""
             if not os.path.isabs(file):
                 out_path = opts.outdir_grids or (opts.outdir_img2img_grids if is_img2img else opts.outdir_txt2img_grids)
                 full_out_path = out_path + "/" + file
+                url = "/file=" + full_out_path
             else:
                 full_out_path = file
-            notice = ""
+                url = "file://" + ("" if file.startswith("/") else "/") + file
+                notice = "<br><span style=\"color: red;\">This is a raw file path, not within the WebUI output directory. You may need to open the output file manually.</span>"
             if os.path.exists(full_out_path):
-                notice = "<br><span style=\"color: red;\">NOTICE: There is already something saved there! This will overwrite prior data.</span>"
-            return f"Page will be at <a style=\"border-bottom: 1px #00ffff dotted;\" href=\"/file={full_out_path}/index.html\" target=\"_blank\" rel=\"noopener noreferrer\">(Click me) <code>{full_out_path}</code></a>{notice}<br><br>"
+                notice += "<br><span style=\"color: red;\">NOTICE: There is already something saved there! This will overwrite prior data.</span>"
+            return f"Page will be at <a style=\"border-bottom: 1px #00ffff dotted;\" href=\"{url}/index.html\" target=\"_blank\" rel=\"noopener noreferrer\">(Click me) <code>{full_out_path}</code></a>{notice}<br><br>"
         def update_page_url(file_path, selected_file):
             out_file_update = gr.Textbox.update()
             if file_path == "" and selected_file == "Create in UI":
